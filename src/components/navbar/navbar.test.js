b2627cb4eeef8df06e9ceeed0f73fecf9c428d1b
Marc Hartmann <107053427+MarcHartmannDev@users.noreply.github.com>
2022-07-10 17:35:52 +0200

    fixed test issues with Router

--- src/components/navbar/navbar.test.js
+++ src/components/navbar/navbar.test.js
@@ -1,9 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./navbar";
 
 describe("Navbar", () => {
   test("renders Navbar", async () => {
-    const { container } = render(<Navbar />);
+    const { container } = render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
 
     expect(container).toBeInTheDocument();
   });
